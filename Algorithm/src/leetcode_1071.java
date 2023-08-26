d033c8592a7b9a9f8d03dd440733372c57b64c42
szszcode <@DESKTOP-LFHG3GF>
2023-08-26 09:11:14 +0900

    leecode_1071

--- Algorithm/src/leetcode_1071.java
+++ Algorithm/src/leetcode_1071.java
@@ -1,40 +1,40 @@
 public class leetcode_1071 {
-    public static String gcdOfStrings(String str1, String str2) {
-        int len1 = str1.length();
-        int len2 = str2.length();
-        int commonLength = gcd(len1, len2);
-        System.out.println(commonLength);
-        String commonStr = str1.substring(0, commonLength);
+	public static String gcdOfStrings(String str1, String str2) {
+		int len1 = str1.length();
+		int len2 = str2.length();
+		int commonLength = gcd(len1, len2);
+		System.out.println(commonLength);
+		String commonStr = str1.substring(0, commonLength);
 
-        if (str1.replaceAll(commonStr, "").isEmpty() && str2.replaceAll(commonStr, "").isEmpty()) {
-        	System.out.println(str1+ " "+str2+"---");
-        	return commonStr;
-        } else {
-            return "";
-        }
-    }
- 
-    public static int gcd(int a, int b) {//최대공ㅇ약수
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        
-        return a;
-    }
+		if (str1.replaceAll(commonStr, "").isEmpty() && str2.replaceAll(commonStr, "").isEmpty()) {
+			System.out.println(str1 + " " + str2 + "---");
+			return commonStr;
+		} else {
+			return "";
+		}
+	}
 
-    public static void main(String[] args) {
-        String str1_1 = "DBD";
-        String str2_1 = "DB";
-        System.out.println(gcdOfStrings(str1_1, str2_1));  // 출력: "ABC"
+	public static int gcd(int a, int b) {// 최대공약수
+		while (b != 0) {
+			int temp = b;
+			b = a % b;
+			a = temp;
+		}
 
-        String str1_2 = "ABABAB";
-        String str2_2 = "ABAB";
+		return a;
+	}
+
+	public static void main(String[] args) {
+		String str1_1 = "DBD";
+		String str2_1 = "DB";
+		System.out.println(gcdOfStrings(str1_1, str2_1)); // 출력: "ABC"
+
+		String str1_2 = "ABABAB";
+		String str2_2 = "ABAB";
 //        System.out.println(gcdOfStrings(str1_2, str2_2));  // 출력: "AB"
 
-        String str1_3 = "LEET";
-        String str2_3 = "CODE";
+		String str1_3 = "LEET";
+		String str2_3 = "CODE";
 //        System.out.println(gcdOfStrings(str1_3, str2_3));  // 출력: ""
-    }
+	}
 }
