f8e06107d50f52eb48e25418d467a058ef100211
szszcode <@DESKTOP-LFHG3GF>
2023-09-12 23:00:41 +0900

    prefix sum_leetcode_1732

--- Algorithm/src/leetcode_1732.java
+++ Algorithm/src/leetcode_1732.java
@@ -1,29 +1,27 @@
 import java.util.Arrays;
 
 public class leetcode_1732 {
-    public static int largestAltitude(int[] gain) {
-    	int[] gain_copy = new int[gain.length+1];
-    	
-    	gain_copy[0] = 0;
-    	for (int i = 0; i < gain.length; i++) {
-    		gain_copy[i+1] = gain[i];
+	public static int largestAltitude(int[] gain) {
+		int[] gain_copy = new int[gain.length + 1];
+
+		gain_copy[0] = 0;
+		for (int i = 0; i < gain.length; i++) {
+			gain_copy[i + 1] = gain[i];
 		}
-    	
-    	int sum = 0, max = -99999;
-    	for (int i = 0; i < gain_copy.length; i++) {
-    		int tmp = gain_copy[i];
-    		sum+=tmp;
-    		gain_copy[i] = sum;
-    		if(max < gain_copy[i]) max = gain_copy[i];
-    	}
-    	System.out.println(Arrays.toString(gain_copy));
-    	System.out.println(max);
-    	
-    	return max;
-    }
-    public static void main(String[] args) {
-		int[] gain = {-5,1,5,0,-7};
+
+		int sum = 0, max = -99999;
+		for (int i = 0; i < gain_copy.length; i++) {
+			int tmp = gain_copy[i];
+			sum += tmp;
+			gain_copy[i] = sum;
+			if (max < gain_copy[i])
+				max = gain_copy[i];
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		int[] gain = { -5, 1, 5, 0, -7 };
 		largestAltitude(gain);
 	}
-    
 }
