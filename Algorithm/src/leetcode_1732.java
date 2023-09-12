3f5707a10a61b73d5f7692d9bbfa791b33e3af5a
szszcode <@DESKTOP-LFHG3GF>
2023-09-12 22:55:01 +0900

    Prefix Sum_leetcode_1732

---
+++ Algorithm/src/leetcode_1732.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class leetcode_1732 {
+    public static int largestAltitude(int[] gain) {
+    	int answer = 0;
+    	int[] gain_copy = new int[gain.length+1];
+    	
+    	gain_copy[0] = 0;
+    	for (int i = 0; i < gain.length; i++) {
+    		gain_copy[i+1] = gain[i];
+		}
+    	
+    	int sum = 0, max = -99999;
+    	for (int i = 0; i < gain_copy.length; i++) {
+    		int tmp = gain_copy[i];
+    		sum+=tmp;
+    		gain_copy[i] = sum;
+    		if(max < gain_copy[i]) max = gain_copy[i];
+    	}
+    	System.out.println(Arrays.toString(gain_copy));
+    	System.out.println(max);
+    	
+    	return answer;
+    }
+    public static void main(String[] args) {
+		int[] gain = {-5,1,5,0,-7};
+		largestAltitude(gain);
+	}
+}
