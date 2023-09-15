efc463cb26568c4f4faba186736194f17706d19e
szszcode <carly4677@gmail.com>
2023-09-15 18:48:50 +0900

    test

--- Algorithm/src/test.java
+++ Algorithm/src/test.java
@@ -1,6 +1,5 @@
-class test{
-	public static void main(String[] args) {
-		System.out.println("test");
+class test {
+    public static void main(String[] args) {
+    	System.out.println("test");
 	}
-	
 }
